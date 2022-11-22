a44af79cf9bfb0b4b541060bfae15f9bee2632cf
Jackson Almeida <jackson.almeida@MacBook-Air-de-Jackson.local>
2022-11-22 17:55:48 +0100

    name added to topnar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,21 @@
 <script setup>
 // imports
-import CartWidget from './CartWidget.vue';
+import CartWidget from "./CartWidget.vue";
+
+import { useAuthUserStore } from "@/stores/AuthUserStore";
+const AuthUserStore = useAuthUserStore();
 </script>
 
 <template>
-  <header 
+  <header
     class="flex justify-between p-6 mb-10 items-center"
     style="background-image: url('/images/double-bubble-outline.png')"
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget/>
+
+    <div>
+      <span class="mr-5">{{ AuthUserStore.username }}</span>
+      <CartWidget class="inline-block" />
+    </div>
   </header>
 </template>
--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,8 @@
 import { defineStore } from "pinia";
+import { groupBy, reduce } from "lodash";
+
+import { useAuthUserStore } from "@/stores/AuthUserStore";
+
 export const useCartStore = defineStore("CartStore", {
   state: () => {
     return {
@@ -9,14 +13,48 @@ export const useCartStore = defineStore("CartStore", {
   getters: {
     count: (state) => state.items.length,
     isEmpty: (state) => state.count === 0,
+
+    /*     crearemos un getter para agrupar por articulo, y lo agruparemos con una funcion de lodash (groupby)  */
+    grouped: (state) => {
+      const grouped = groupBy(state.items, (item) => item.name);
+
+      /* luego guardaremos este, en un variable llamada sorted, la cual ordenara por keys.
+      Meteremos cada calor de sorted, en inOrder y lo devolveremos. */
+
+      const sorted = Object.keys(grouped).sort();
+      let inOrder = {};
+      sorted.forEach((key) => (inOrder[key] = grouped[key]));
+      return inOrder;
+    },
+
+    groupCount: (state) => (name) => state.grouped[name].length,
+
+    /* total price of cart */
+    total: (state) => state.items.reduce((a, b) => a + b.price, 0),
   },
 
   actions: {
+    checkout() {
+      const authUserStore = useAuthUserStore();
+      alert(
+        `${authUserStore.username} just bought ${this.count} items at a total of $${this.total}`
+      );
+    },
+
     addItems(count, item) {
       count = parseInt(count);
       for (let index = 0; index < count; index++) {
         this.items.push({ ...item });
       }
+    },
+
+    clearItem(itemName) {
+      this.items = this.items.filter((item) => item.name !== itemName);
+    },
+
+    setItemCount(item, count) {
+      this.clearItem(item.name);
+      this.addItems(count, item);
     },
   },
 });
